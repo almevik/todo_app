0e8c70ab2c81ca7f4ad50dfa6340cf83f5056e61
a.medvedev <a.medvedev@smter.ru>
2019-01-13 10:30:56 +0300

    Валидация данных формы.

--- vue/components/NoteAdd.vue
+++ vue/components/NoteAdd.vue
@@ -2,17 +2,32 @@
     <div class="note">
         <h1>Добавление заметки</h1>
         <form>
+            <p v-if="errors.length">
+                <b>Пожалуйста исправьте указанные ошибки:</b>
+                <ul>
+                    <li v-for="error in errors">{{ error }}</li>
+                </ul>
+            </p>
             <div class="form-group">
                 <label for="title">Название</label>
-                <input type="text" class="form-control" id="title" placeholder="Введите заголовок" v-model="note.title" />
+                <input type="text"
+                       class="form-control"
+                       id="title"
+                       name="title"
+                       placeholder="Введите заголовок"
+                       required
+                       v-model="note.title"/>
             </div>
             <div class="form-group">
                 <label for="date">Дата</label>
-                <input type="text" class="form-control" id="date" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}" v-model="note.date" />
+                <input type="text" class="form-control" id="date" name="date" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"
+                       required
+                       v-model="note.date"/>
             </div>
             <div class="form-group">
                 <label for="text">Текст заметки</label>
-                <input type="text" class="form-control" id="text" placeholder="Текст заметки" v-model="note.text" />
+                <input type="text" class="form-control" id="text" name="text" placeholder="Текст заметки"
+                       v-model="note.text"/>
             </div>
             <div class="form-group">
                 <button type="submit" class="btn btn-primary" @click.prevent="addNote()">Создать</button>
@@ -22,43 +37,50 @@
 </template>
 
 <script>
-import axios from 'axios';
-    
-export default {
-    data() {
-        return {
-            note: {
-                title: 'test',
-                date: '2019-01-10',
-                text: 'Заметка 1',
+    import axios from 'axios';
+
+    export default {
+        data() {
+            return {
+                errors: [],
+                note: {
+                    title: 'test',
+                    date: '2019-01-10',
+                    text: 'Заметка 1',
+                }
             }
-        }
-    },
-    methods: {
-        addNote() {
-            axios({
-                    method: 'post',
-                    url: '/api/notes',
-                    responseType: 'json',
-                    data: this.note,
-                    headers: {
-                        'Content-type': 'application/json; charset=UTF-8',
+        },
+        methods: {
+            addNote() {
+                this.errors = [];
+                axios({
+                        method: 'post',
+                        url: '/api/notes',
+                        responseType: 'json',
+                        data: this.note,
+                        headers: {
+                            'Content-type': 'application/json; charset=UTF-8',
                         }
                     }
                 )
-              .then(response => (
-                    //this.note = response.data
-                    this.$router.push('/')
+                    .then(response => (
+                            //this.note = response.data
+                            this.$router.push('/')
+                        )
                     )
-                )
-              .catch(error => console.log(error));
-      
-            
-        }
-    },   
+                    // .catch(error => console.log(error));
+                    .catch(error => {
+                        let errors = [];
+                        $.each(error.response.data, function(key, value) {
+                            // list.push(key);
+                            errors.push(value.field + ': ' + value.message);
+                        });
+                        this.errors.push(errors);
+                    });
 
-    
-    }    
+            }
+        },
+    }
 </script>
 
 <style scoped>
@@ -66,6 +88,6 @@ export default {
         border: 1px solid grey;
         border-radius: 10px;
         margin: 10px 0;
-        padding: 10px;  
+        padding: 10px;
     }
 </style>
